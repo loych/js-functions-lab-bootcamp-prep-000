1131c5dbaaac37360cbbf835c0db19f09d1e3bba
insil321@hotmail.com <insil321@hotmail.com>
2018-09-12 05:50:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function happyHolidaysTo(name){
   return happyHolidaysTo(`"Happy holidays", ${name}!`)
 }
 happyHolidaysTo("Happy holidays", Lisa)
+
+function happyHolidayTo(holiday, name){
+  return happyHolidayTo(`Happy ${holiday}, ${name}!`)
+}
+happyHolidayTo("Christmas","Lisa")
