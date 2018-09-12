ecd22af2f88d9d636a5887debfcd1b055d4c65de
insil321@hotmail.com <insil321@hotmail.com>
2018-09-12 05:56:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 // write your code below!
 function happyHolidays(){
-  return "Happy Holidays!"
+  return "Happy holidays!"
 }
 
 function happyHolidaysTo(name){
   return happyHolidaysTo(`"Happy holidays", ${name}!`)
 }
-happyHolidaysTo("Happy holidays", Lisa)
 
 function happyHolidayTo(holiday, name){
   return happyHolidayTo(`Happy ${holiday}, ${name}!`)
 }
-happyHolidayTo("Christmas","Lisa")
 
 function holidayCountdown(holiday, numberday){
   return holidayCountdown(`It\'s ${numberday} days until ${holiday}!`)
 }
-
-holidayCountdown("Christmas", 5)
