df583778dce693e536acdd8c917bbb1664272459
insil321@hotmail.com <insil321@hotmail.com>
2018-09-12 05:57:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  return happyHolidaysTo(`"Happy holidays", ${name}!`)
+  return (`"Happy holidays", ${name}!`)
 }
 
 function happyHolidayTo(holiday, name){
-  return happyHolidayTo(`Happy ${holiday}, ${name}!`)
+  return (`Happy ${holiday}, ${name}!`)
 }
 
 function holidayCountdown(holiday, numberday){
-  return holidayCountdown(`It\'s ${numberday} days until ${holiday}!`)
+  return (`It\'s ${numberday} days until ${holiday}!`)
 }
